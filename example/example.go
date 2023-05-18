f1a6a6272b1f170207092d0d46aeb77b87b0313d
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 00:27:01 +0200

    Updated documentation for logger

--- example/example.go
+++ example/example.go
@@ -8,25 +8,36 @@ func init() {
 	// Configuration for the logger must be written in the init function!
 	// Configuration can be set by assigning a new 'logger.LoggerConfig' struct to the global variable 'logger.Config'.
 	// Or by setting the fields of 'logger.Config' directly like 'logger.Config.Out = logger.ALL'.
+
+	// 'InDebug' option controls whether the logger can use the 'logger.Debug' and 'logger.DebugRGB' logging function.
+	// If 'InDebug' is set to false than debugging functions will give a warn level message saying 'NOT IN DEBUG MODE!'
+
 	// 'Out' in 'logger.LoggerConfig' is the target output for logging.
 	// Outputs can be: 'FILE', 'TERMINAL', 'ALL'
 
-	// 'FILE' option creates a 'logs' directory in the scope of the entry point where the logger is started,
+	// 'logger.FILE' option creates a 'logs' directory in the scope of the entry point where the logger is started,
 	// and the logging functions will write to a log file in it, every run a new log file is created.
 
-	// 'TERMINAL' option logs to the terminal directly.
+	// 'logger.TERMINAL' option logs to the terminal directly.
 
-	// 'ALL' option combines the behaviour of all the other options (called 'ALL' incase of adding new logging targets).
+	// 'logger.ALL' option combines the behaviour of all the other options (called 'ALL' incase of adding new logging targets).
 
-	// The field 'Server' is for future use currently it doesn't do anything right now!
+	// The field 'Server' is for future use, currently it doesn't do anything!
+
+	// If the Config for the logger isn't set than the logger will use the base configurations, which are the following:
+	//
+	// InDebug: false
+	// Out: logger.TERMINAL
+	// Server: ""
 
 	logger.Config = &logger.LoggerConfig{
-		Out:    logger.ALL,
-		Server: "",
+		InDebug: true,
+		Out:     logger.ALL,
+		Server:  "",
 	}
 
 	// After setting the configurations 'logger.Start()' must be called to start logging,
-	// otherwise the logging with option 'FILE' won't work!
+	// otherwise the logging with option 'logger.FILE' won't work!
 	logger.Start()
 }
 
@@ -38,7 +49,9 @@ func main() {
 	logger.Info("INFO", "dataInfo")
 	logger.Warn("WARN", "dataWarn", "dataWarn1")
 	logger.Error("ERROR", "dataErr")
-	logger.Fatal("FATAL", "panic value", "data", "data1", "data2")
 	logger.Debug("DEBUG")
 	logger.DebugRGB("DEBUG_RGB", logger.RGBCode{R: 3, G: 252, B: 107})
+
+	// 'logger.Fatal' is the only logging funtion that will stop the execution of a goroutine
+	logger.Fatal("FATAL", "panic value", "data", "data1", "data2")
 }
