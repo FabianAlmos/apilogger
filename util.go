796eb5daff86eee9ff8afbce22f04721e4941cd5
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 00:02:23 +0200

    Fixed data logging

--- util.go
+++ util.go
@@ -11,17 +11,25 @@ type RGBCode struct {
 	R, G, B escCode
 }
 
-func addData(data string) string {
-	if data != "" {
-		return "\n\t" + data
+func addData(data ...any) string {
+	res := ""
+	for index, d := range data {
+		if index < len(data)-1 {
+			res += "\t" + fmt.Sprintln(d)
+		} else {
+			res += "\t" + fmt.Sprint(d)
+		}
+	}
+	if res != "" {
+		return "\n" + res
 	}
 	return ""
 }
 
-func constructLogFormat(level logLevel, msg string, data string) string {
+func constructLogFormat(level logLevel, msg string, data ...any) string {
 	time := time.Now().Format("2006-01-02-15:04:05 MST")
 	if level == info || level == warn {
-		return fmt.Sprintf("[ %s  - %s ]:\t", level, time) + msg + addData(data)
+		return fmt.Sprintf("[ %s  - %s ]:\t", level, time) + msg + addData(data...)
 	}
-	return fmt.Sprintf("[ %s - %s ]:\t", level, time) + msg + addData(data)
+	return fmt.Sprintf("[ %s - %s ]:\t", level, time) + msg + addData(data...)
 }
