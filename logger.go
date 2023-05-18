0fe7d250ca7955db6d7385e466131136b3519e19
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 00:39:36 +0200

    Added logging functions

--- logger.go
+++ logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"fmt"
+	"time"
 )
 
 type escCode uint8
@@ -26,6 +27,14 @@ type RGBCode struct {
 	r, g, b escCode
 }
 
+type logLevel string
+
+const (
+	info   logLevel = "INFO"
+	warn   logLevel = "WARN"
+	error_ logLevel = "ERROR"
+)
+
 func optionBuilder(codes ...escCode) string {
 	res := "\033["
 	for index, code := range codes {
@@ -49,4 +58,24 @@ func msgBuilderRGB(msg string, code escCode, rgb RGBCode) string {
 	resetOption := optionBuilder(reset)
 	style := optionBuilder(code, dim, rgb.r, rgb.g, rgb.b)
 	return style + msg + resetOption
+}
+
+func constructLogFormat(level logLevel, msg string) string {
+	time := time.Now().Format("2006-01-02 - 15:04:05 MST")
+	return fmt.Sprintf("[ %s - %s ]:\t", level, time) + msg
+}
+
+func Info(msg string) {
+	message := constructLogFormat(info, msg)
+	fmt.Println(msgBuilder(message, foreground, slowblink, AQUA))
+}
+
+func Warn(msg string) {
+	message := constructLogFormat(warn, msg)
+	fmt.Println(msgBuilder(message, foreground, slowblink, YELLOW))
+}
+
+func Error(msg string) {
+	message := constructLogFormat(error_, msg)
+	fmt.Println(msgBuilder(message, foreground, slowblink, RED))
 }
