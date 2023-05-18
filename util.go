e79d21c991853939f8e5f6eafd9f63718d560bc5
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 00:41:24 +0200

    Made addData ternary function

--- util.go
+++ util.go
@@ -11,10 +11,17 @@ type RGBCode struct {
 	R, G, B escCode
 }
 
+func addData(data string) string {
+	if data != "" {
+		return "\n\t" + data
+	}
+	return ""
+}
+
 func constructLogFormat(level logLevel, msg string, data string) string {
 	time := time.Now().Format("2006-01-02-15:04:05 MST")
 	if level == info || level == warn {
-		return fmt.Sprintf("[ %s  - %s ]:\t", level, time) + msg + "\n\t" + data
+		return fmt.Sprintf("[ %s  - %s ]:\t", level, time) + msg + addData(data)
 	}
-	return fmt.Sprintf("[ %s - %s ]:\t", level, time) + msg + "\n\t" + data
+	return fmt.Sprintf("[ %s - %s ]:\t", level, time) + msg + addData(data)
 }
