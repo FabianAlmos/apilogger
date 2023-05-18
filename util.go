930e07edacb88d6f11ad57af5f5106324733638a
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 00:40:39 +0200

    Separated code of logger

---
+++ util.go
@@ -0,0 +1,17 @@
+package logger
+
+import (
+	"fmt"
+	"time"
+)
+
+type escCode uint8
+type logLevel string
+type RGBCode struct {
+	R, G, B escCode
+}
+
+func constructLogFormat(level logLevel, msg string) string {
+	time := time.Now().Format("2006-01-02-15:04:05 MST")
+	return fmt.Sprintf("[ %s\t- %s ]:\t", level, time) + msg
+}
