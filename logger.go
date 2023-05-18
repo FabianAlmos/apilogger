1c98cf0b5a38b46c1376170e3d1fbfae982e858f
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 00:39:22 +0200

    Added ANSI support functions

--- logger.go
+++ logger.go
@@ -1,1 +1,33 @@
 package logger
+
+import "fmt"
+
+type escCode int8
+type msgOption string
+
+const (
+	reset      escCode = 0
+	dim        escCode = 2
+	slowblink  escCode = 5
+	foreground escCode = 38
+	background escCode = 48
+)
+
+func optionBuilder(codes ...escCode) msgOption {
+	res := "\033["
+	for index, code := range codes {
+		if index < len(codes)-1 {
+			res += fmt.Sprintf("%d", code) + ";"
+		} else {
+			res += fmt.Sprintf("%d", code)
+		}
+	}
+	res += "m"
+	return msgOption(res)
+}
+
+func msgBuilder(msg string, codes ...escCode) string {
+	resetOption := string(optionBuilder(reset))
+	style := string(optionBuilder(codes...))
+	return style + msg + resetOption
+}
